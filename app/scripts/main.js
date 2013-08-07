2307ed892b17703a7c4e2577bb5f30f66e72bc85
frob <frob@249517.no-reply.drupal.org>
2013-08-06 23:30:16 -0700

    changed a bunch of text and added a bunch of bower stuff that I thought was already added.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,6 +14,10 @@ require.config({
 require(['app', 'jquery', 'bootstrap'], function (app, $) {
     'use strict';
     // use app here
-    console.log(app);
+    if(app) {
+        console.log("Hello, if you are reading this then I guess you are interested in how this site works.");
+        console.log("You will have a hard time reading the Javascript and css for this site because it has all been optimized.");
+        console.log("The site is built with Yeoman and the source is available on github. I suggest you look there.");
+    }
     console.log('Running jQuery %s', $().jquery);
 });
